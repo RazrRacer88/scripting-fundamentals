39474074671e9abcff954611cd255ec7f90ebfc4
RzrRacer88 <Georgellamosas@hotmail.com>
2023-10-03 14:22:18 -0700

    creating new variables

---
+++ My project/Assets/Scripts/Variables.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    // Public Global/Members Variables
+    public int Score = 0; // declare a whole number
+    public float ItemAmount = 19.95f; // declare decimal number
+    public string PlayerName = "John Doe"; //declare a text
+    public bool IsPlayerActive = true; //declare a true/false statement.
+
+    //Private Global/Member Variables
+    private int _collectivbleAmount = 0; // declare a whole number
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
