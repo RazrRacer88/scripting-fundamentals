420f3c74f0b892a460597f4704c7a9d06623534b
RzrRacer88 <Georgellamosas@hotmail.com>
2023-10-04 13:12:24 -0700

    Updating Variable File

--- My project/Assets/Scripts/Variables.cs
+++ My project/Assets/Scripts/Variables.cs
@@ -12,15 +12,26 @@ public class Variables : MonoBehaviour
 
     //Private Global/Member Variables
     private int _collectivbleAmount = 0; // declare a whole number
+    private float _horizontalInput = 0f;
+    private string _UserName = "John007";
+    private bool isGameOver = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //make a  Score Variable is = 0
+        Score = 0; //assigning a Global/Member variable
+        // declare a local variable to track Starty Position
+        Vector3 startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //trying to update a local variables value
+        // we can not update a loca lvariable
+        //startPosition = new vector(Vector.right);
+
+        //Create a variable to determine object key movement
+        float verticalInput = Input.GetAxis("Vertical");
     }
 }
